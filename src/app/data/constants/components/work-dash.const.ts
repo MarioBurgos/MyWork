a6ef23713d63eaeeebffc78a6295682307aceb4c
Mario Burgos <mario.burpi@gmail.com>
2021-10-06 22:10:03 +0200

    added information on cv.component.html

--- src/app/data/constants/components/work-dash.const.ts
+++ src/app/data/constants/components/work-dash.const.ts
@@ -3,7 +3,7 @@ export const WORK_DATA= {
   IMG_ANGULAR_LOGO: 'assets/img/angular_logo.png',
   IMG_OTHER_PROJECTS: 'assets/img/other_projects.png',
   CARD_DESCRIPTION_ANGULAR:  'My highlighted frontend projects using Angular',
-  CARD_DESCRIPTION_OTHER_PROJECTS: 'Interesting projects coded in other languages (mostly Java)'
+  CARD_DESCRIPTION_OTHER_PROJECTS: 'Interesting projects coded in other languages'
 
 
 }
